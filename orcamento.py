b1ed177664ec7d62e8b85fbaeec08a8d3e28c4c9
mcmacedo <mcmacedoprog@outlook.com>
2017-09-14 20:58:04 -0300

    Implementada a classe Item e refatorada a classe Orcamento

--- orcamento.py
+++ orcamento.py
@@ -2,14 +2,58 @@
 
 class Orcamento(object):
     """
-    Armazena um orçamento, cálculo ou estimativa de custos.
+    Representa um orçamento formado por uma lista de items.
     """
-    def __init__(self, valor):
+    def __init__(self):
+        self.__itens = []
+
+    @property
+    def valor(self):
+        """
+        Retorna a soma dos valores dos itens do orçamento. 
+        """
+        total = 0.0
+        for item in self.__itens:
+            total += item.valor
+        return total
+
+    @property
+    def total_itens(self):
+        """
+        Retorna quantidade de itens do orçamento.
+        """
+        return len(self.__itens)
+
+    def obter_itens(self):
+        """
+        Retorna tupla com os itens do orçamento.
+        """
+        return tuple(self.__itens)
+
+    def adiciona_item(self, item):
+        """
+        Adiciona um item ao orçamento
+        """
+        self.__itens.append(item)
+
+class Item(object):
+    """
+    Item ou produto.
+    """
+    def __init__(self, nome, valor):
+        self.__nome = nome
         self.__valor = valor
 
     @property
     def valor(self):
         """
-        Getter para a propriedade valor.
+        Getter do atributo valor.
         """
         return self.__valor
+
+    @property
+    def nome(self):
+        """
+        Getter do atributo nome.
+        """
+        return self.__nome
